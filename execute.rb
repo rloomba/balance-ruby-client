bbddb00c1c584fa8eb06daefc24988473ade7bd0
Ryan Loomba <ryan.loomba@gmail.com>
2013-05-10 11:51:00 -0700

    wip

--- execute.rb
+++ execute.rb
@@ -1,13 +1,21 @@
 require 'json'
 require 'net/http'
 
-data_file = File.read("./data.json")
+data_file = File.read("./scenario.cache")
 parsed_data = JSON.parse(data_file)
 
 Dir.chdir("./scenarios")
 subdir_list = Dir["*"].reject { |o| not File.directory?(o) }
-
+ruby_response_hash = {}
 subdir_list.each do |scenario|
-  response = eval(File.read("./account_create/executable.rb"))
-  puts response ? "#{scenario} VALID" : "#{scenario}: INVALID"
+  begin
+    response = eval(File.read("./#{scenario}/executable.rb"))
+    ruby_response_hash[scenario] = response
+  rescue
+    puts "#{scenario}'s code does not execute"
+  end
 end
+File.open("../responses.rb", "w") do |f|
+      f.write(ruby_response_hash.to_json)
+end
+
--- render.rb
+++ render.rb
@@ -3,20 +3,22 @@ require 'json'
 
 data_file = File.read("./scenario.cache")
 parsed_data = JSON.parse(data_file)
-# puts parsed_data['account_add_card']['metadata']
 Dir.chdir("./scenarios")
-# subdir_list = Dir["*"].reject { |o| not File.directory?(o) }
-subdir_list = ['refund_update']
+subdir_list = Dir["*"].reject { |o| not File.directory?(o) }
+# subdir_list = ['refund_update']
+boiler_plate = "require 'balanced'\nBalanced.configure('03921a2cb69311e28b89026ba7c1aba6')\n"
 
 subdir_list.each do |scenario|
-  boiler_plate = "require 'balanced'\nBalanced.configure('03921a2cb69311e28b89026ba7c1aba6')\n"
-  definition = File.read("./#{scenario}/definition.rb")
-  template = File.read("./#{scenario}/request.rb")
-  erb = ERB.new(template)
-  request = parsed_data[scenario]['request']
-  payload = request['payload']
-  executable = erb.result(binding)
-  File.open("./#{scenario}/executable.rb", "w") do |f|
-    f.write(executable)
+  begin
+    template = File.read("./#{scenario}/request.rb")
+    erb = ERB.new(template)
+    request = parsed_data[scenario]['request']
+    payload = request['payload']
+    executable = erb.result(binding)
+    File.open("./#{scenario}/executable.rb", "w") do |f|
+      f.write(executable)
+    end
+  rescue Exception => e
+    puts "#{scenario} does not contain a valid template file"
   end
 end
